9c931ccd4e4c2cab1b94e2361f4ee22d2825759f
evilebottnawi <sheo13666q@gmail.com>
2018-10-12 22:22:33 +0300

    chore: prepare `2.2.7` release

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -29,13 +29,22 @@ class ComposerAutoloaderInit7844ca910a4e686d4bee3bc291a65c85
 
             call_user_func(\Composer\Autoload\ComposerStaticInit7844ca910a4e686d4bee3bc291a65c85::getInitializer($loader));
         } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         return $loader;
